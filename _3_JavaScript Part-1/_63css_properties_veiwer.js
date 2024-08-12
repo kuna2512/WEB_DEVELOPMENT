af14f19a58da1bd570c2ab58bf63647b1f0b3af7
kuna2512 <129643132+kuna2512@users.noreply.github.com>
2024-08-12 22:36:38 +0530

    Java Script

---
+++ _3_JavaScript Part-1/_63css_properties_veiwer.js
@@ -0,0 +1,24 @@
+function color(){
+    var x=document.getElementById("result");
+    x.style.color="red";
+}
+function font_family(){
+    var x=document.getElementById("result");
+    x.style.fontFamily="calibari";  
+}
+function bbold(){
+    var x=document.getElementById("result");
+    x.style.fontWeight="bold";
+}
+function sized(){
+    var x=document.getElementById("result");
+    x.style.fontSize="70px";
+}
+function its(){
+    var x=document.getElementById("result");
+    x.style.fontStyle="italic";
+}
+function bg(){
+    var x=document.getElementById("brand-name");
+    x.style.backgroundColor="#ccc";
+}
---
+++ _3_JavaScript Part-1/_64css_properties_veiwer.js
@@ -0,0 +1,4 @@
+function bg(){
+    var x=document.getElementById("col-11");
+    x.style.backgroundColor="yellow";
+}
